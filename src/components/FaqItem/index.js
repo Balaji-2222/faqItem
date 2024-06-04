9e403dd8b0f1bc5cdb62bac233c3500be6e000cd
Balaji-2222 <balisaibalaji.2003@gmail.com>
2024-06-04 11:22:19 +0000

    Solution

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,19 +1,32 @@
-const FaqList = props => {
-  const {eachItem, changeColor, para, imageUrl, alterText} = props
-  const {id, questionText} = eachItem
-  const changeButton = () => {
-    changeColor(id)
+import {Component} from 'react'
+
+class FaqList extends Component {
+  state = {isColorChange: false}
+
+  changeColor = () => {
+    const {isColorChange} = this.state
+    this.setState(prevState => ({isColorChange: !prevState.isColorChange}))
   }
 
-  return (
-    <li id={id}>
-      <h1>{questionText}</h1>
-      <button onClick={changeButton} type="button">
-        <img src={imageUrl} alt={alterText} />
-      </button>
-      <p>{para}</p>
-    </li>
-  )
+  render() {
+    const {eachItem} = this.props
+    const {id, questionText} = eachItem
+    const {isColorChange} = this.state
+    const imageUrl = isColorChange
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png '
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+    const imageText = isColorChange ? 'Plus' : 'Minus'
+    const paraText = isColorChange ? eachItem.answerText : ''
+    return (
+      <li id={id}>
+        <h1>{questionText}</h1>
+        <button type="button" onClick={this.changeColor}>
+          <img src={imageUrl} alt={imageText} />
+        </button>
+        <p>{paraText}</p>
+      </li>
+    )
+  }
 }
 
 export default FaqList
